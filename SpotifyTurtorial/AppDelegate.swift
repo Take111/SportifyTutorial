2e1ea8f18e3f0d9ffdf1e2fd4eeb900dc691e3a2
Take111 <maxmaxastea@gmail.com>
2020-03-11 23:33:43 +0900

    feat: add

--- SpotifyTurtorial/AppDelegate.swift
+++ SpotifyTurtorial/AppDelegate.swift
@@ -11,10 +11,10 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        
         return true
     }
 
---
+++ SpotifyTutorial-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SpotifyTutorial-Bridging-Header.h
+//  SpotifyTurtorial
+//
+//  Created by 竹ノ内愛斗 on 2020/03/11.
+//  Copyright © 2020 竹ノ内愛斗. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#ifndef SpotifyTutorial_Bridging_Header_h
+#define SpotifyTutorial_Bridging_Header_h
+
+
+#endif /* SpotifyTutorial_Bridging_Header_h */
